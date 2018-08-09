3bb981dc0232ea4071b582400155d204b929447a
matsuhashi <matsuhashi@matsuhashinoiMac.local>
2018-08-09 11:07:22 +0900

    header修正

--- themes/theme/header.php
+++ themes/theme/header.php
@@ -16,10 +16,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
 	<?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'theme' ); ?></a>
@@ -33,14 +31,14 @@
 					<button class="menu-toggle hamburger" aria-controls="primary-menu" aria-expanded="false"><span class="hamburger_top"></span><span class="hamburger_middle"></span><span class="hamburger_bottom"></span></button>
 					<div id="primary-menu" class="menu">
 						<ul aria-expanded="true" class="nav-menu">
-							<li class=""><div class="js-scroll-area-togled" data-area="#works"><a class="works" href="">作品紹介<small>works</small></a></div></li>
-							<li class=""><div class="js-scroll-area-togled" data-area="#about"><a class="about" href="">会社案内<small>company</small></a></div></li>
-							<li class=""><div class="js-scroll-area-togled" data-area="#contact"><a class="contact" href="">お問い合わせ<small>contact</small></a></div></li>
-							<li class="primary-menu__phone">
+							<li><div class="js-scroll-area-togled" data-area="#works"><a class="works" href="">作品紹介<small>works</small></a></div></li>
+							<li><div class="js-scroll-area-togled" data-area="#about"><a class="about" href="">会社案内<small>company</small></a></div></li>
+							<li><div class="js-scroll-area-togled" data-area="#contact"><a class="contact" href="">お問い合わせ<small>contact</small></a></div></li>
+							<li class="m_display_none">
 								<?php if( wp_is_mobile() ): ?>
-									<a class="phone" href="tel:000-000-0000">000-000-0000</a>
+									<a class="phone no-caption" href="tel:000-000-0000">000-000-0000</a>
 								<?php else: ?>
-									<span>000-000-0000</span>
+									<span class="phone no-caption">000-000-0000</span>
 								<?php endif; ?>
 							</li>
 						</ul>
--- themes/theme/js/origin/component.js
+++ themes/theme/js/origin/component.js
@@ -2,16 +2,16 @@
 * ページ内 スムーズスクロール
 ================================ */
 jQuery(function () {
-    jQuery('a[href^="#"]').click(function () {
-        /* スクロールの速度（ミリ秒） */
-        var speed = 2000;
+  jQuery('a[href^="#"]').click(function () {
+    /* スクロールの速度（ミリ秒） */
+    var speed = 2000;
 
-        var href = jQuery(this).attr("href");
-        var target = jQuery(href === "#" || href === "" ? 'html' : href);
-        var position = target.offset().top;
-        jQuery('body,html').animate({scrollTop: position}, speed, 'swing');
-        return false;
-    });
+    var href = jQuery(this).attr("href");
+    var target = jQuery(href === "#" || href === "" ? 'html' : href);
+    var position = target.offset().top;
+    jQuery('body,html').animate({scrollTop: position}, speed, 'swing');
+    return false;
+  });
 });
 
 
@@ -21,15 +21,47 @@ jQuery(function () {
 ================================ */
 jQuery(function () {
 
-    jQuery(document).ready(function() {
-      jQuery('body').append('<div id="opening_anime" class="opening_anime"><div class="opening_anime__inner"></div></div>');
-    });
+  jQuery(document).ready(function() {
+    jQuery('body').append('<div id="opening_anime" class="opening_anime"><div class="opening_anime__inner"></div></div>');
+  });
 
-    jQuery(window).on('load', function() {
-      jQuery('#opening_anime')[0].classList.add('end');
-    });
+  jQuery(window).on('load', function() {
+    jQuery('#opening_anime')[0].classList.add('end');
+  });
 
 });
+
+
+
+/* ================================
+* スクロール位置検知
+* （スクロール位置が最上部以外の時にbodyに.do_scrollを付与）
+================================ */
+jQuery(function(){
+
+  var scroll_check = (function(){
+    if( jQuery(window).scrollTop() <= 0){
+      jQuery("body")[0].classList.remove("do_scroll");
+    }else{
+      jQuery("body")[0].classList.add("do_scroll");
+    }
+  });
+
+  jQuery(window).on( "load", scroll_check );
+  jQuery(window).scroll( scroll_check );
+
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -46,39 +78,39 @@ jQuery(function () {
 * data-toggle-append-parentが指定されていて場合、toggle-on時は自身の親要素の
 * （アコーディオンなどで使用を想定）
 ================================ */
-jQuery(function () {
-  jQuery('.js-toggled').on('click', function(event){
-
-    var target = jQuery(this);
-
-    /* 親要素に付ける場合はtargetを変更する */
-    if( jQuery(this).data("toggle-target-parent") ){
-      target = jQuery(this).parent();
-    }
-
-    /* 現在の状態を取得(.toggle-onが設定されている場合はtrue、されていない場合はfalse) */
-    var target_state = target.hasClass("toggle-on");
-
-    /* 同一codeのtoggle-onを消す */
-    var code = jQuery(this).data("toggle-code");
-    if( code ){
-      jQuery('.js-toggled[data-toggle-code=' + code + ']').each(function(){
-        var target = jQuery(this);
-        if( jQuery(this).data("toggle-target-parent") ){
-          target = jQuery(this).parent();
-        }
-        target.removeClass( "toggle-on" );
-      });
-    }
-
-    /* toggle-on をオン・オフする*/
-    if( target_state ){
-      target.removeClass( "toggle-on" );
-    }else{
-      target.addClass( "toggle-on" );
-    }
-  });
-});
+// jQuery(function () {
+//   jQuery('.js-toggled').on('click', function(event){
+//
+//     var target = jQuery(this);
+//
+//     /* 親要素に付ける場合はtargetを変更する */
+//     if( jQuery(this).data("toggle-target-parent") ){
+//       target = jQuery(this).parent();
+//     }
+//
+//     /* 現在の状態を取得(.toggle-onが設定されている場合はtrue、されていない場合はfalse) */
+//     var target_state = target.hasClass("toggle-on");
+//
+//     /* 同一codeのtoggle-onを消す */
+//     var code = jQuery(this).data("toggle-code");
+//     if( code ){
+//       jQuery('.js-toggled[data-toggle-code=' + code + ']').each(function(){
+//         var target = jQuery(this);
+//         if( jQuery(this).data("toggle-target-parent") ){
+//           target = jQuery(this).parent();
+//         }
+//         target.removeClass( "toggle-on" );
+//       });
+//     }
+//
+//     /* toggle-on をオン・オフする*/
+//     if( target_state ){
+//       target.removeClass( "toggle-on" );
+//     }else{
+//       target.addClass( "toggle-on" );
+//     }
+//   });
+// });
 
 
 /* ================================
@@ -91,38 +123,38 @@ jQuery(function () {
 * 注意：onが付与された要素に対してposition等位置が変化するcssを適用しないこと
 ※     サイズ変更などが発生する場合はdelayを使うなどして、場所が変わる事を意識する
 ================================ */
-jQuery(function(){
-  jQuery(window).on("load",function(){
-    jQuery(".js-scroll-togled").each(function(){
-      var toggled_timing = jQuery(this).attr('data-toggled-timing');
-      if( toggled_timing === undefined || ( toggled_timing !== 'page-top' && toggled_timing !== 'page-bottom' ) ){
-        jQuery(this).attr( 'data-toggled-timing', 'page-top' );
-      }
-      if( jQuery(this).attr('data-toggled-delay') === undefined ){
-        jQuery(this).attr( 'data-toggled-delay', 0 );
-      }
-    });
-  });
-
-  jQuery(window).scroll(function(){
-    jQuery(".js-scroll-togled[data-toggled-timing=page-top]").each(function(){
-       if( jQuery(window).scrollTop() - Number( jQuery(this).offset().top ) > Number( jQuery(this).attr('data-toggled-delay') ) ){
-         jQuery(this).addClass('on');
-       }else{
-         jQuery(this).removeClass('on');
-       }
-    });
-
-    jQuery(".js-scroll-togled[data-toggled-timing=page-bottom]").each(function(){
-      if( (jQuery(window).scrollTop() + window.innerHeight ) - ( Number( jQuery(this).offset().top ) + jQuery(this).height() ) > Number( jQuery(this).attr('data-toggled-delay') ) ){
-        jQuery(this).addClass('on');
-      }else{
-        jQuery(this).removeClass('on');
-      }
-
-    });
-  });
-});
+// jQuery(function(){
+//   jQuery(window).on("load",function(){
+//     jQuery(".js-scroll-togled").each(function(){
+//       var toggled_timing = jQuery(this).attr('data-toggled-timing');
+//       if( toggled_timing === undefined || ( toggled_timing !== 'page-top' && toggled_timing !== 'page-bottom' ) ){
+//         jQuery(this).attr( 'data-toggled-timing', 'page-top' );
+//       }
+//       if( jQuery(this).attr('data-toggled-delay') === undefined ){
+//         jQuery(this).attr( 'data-toggled-delay', 0 );
+//       }
+//     });
+//   });
+//
+//   jQuery(window).scroll(function(){
+//     jQuery(".js-scroll-togled[data-toggled-timing=page-top]").each(function(){
+//        if( jQuery(window).scrollTop() - Number( jQuery(this).offset().top ) > Number( jQuery(this).attr('data-toggled-delay') ) ){
+//          jQuery(this).addClass('on');
+//        }else{
+//          jQuery(this).removeClass('on');
+//        }
+//     });
+//
+//     jQuery(".js-scroll-togled[data-toggled-timing=page-bottom]").each(function(){
+//       if( (jQuery(window).scrollTop() + window.innerHeight ) - ( Number( jQuery(this).offset().top ) + jQuery(this).height() ) > Number( jQuery(this).attr('data-toggled-delay') ) ){
+//         jQuery(this).addClass('on');
+//       }else{
+//         jQuery(this).removeClass('on');
+//       }
+//
+//     });
+//   });
+// });
 
 
 /* ================================
@@ -136,95 +168,95 @@ jQuery(function(){
 * 注意：onが付与された要素に対してposition等位置が変化するcssを適用しないこと
 ※     サイズ変更などが発生する場合はdelayを使うなどして、場所が変わる事を意識する
 ================================ */
-jQuery(function(){
-  var scroll_toggled_event = function(){
-
-    jQuery(".js-scroll-togled[data-toggled-timing=page-top]").each(function(){
-       if( jQuery(window).scrollTop() - Number( jQuery(this).offset().top ) > Number( jQuery(this).attr('data-top-toggled-delay') ) ){
-         jQuery(this).addClass('top-on');
-       }else{
-         jQuery(this).removeClass('top-on');
-       }
-    });
-
-    jQuery(".js-scroll-togled[data-toggled-timing=page-bottom]").each(function(){
-      if( (jQuery(window).scrollTop() + window.innerHeight ) - ( Number( jQuery(this).offset().top ) + jQuery(this).height() ) > Number( jQuery(this).attr('data-bottom-toggled-delay') ) ){
-        jQuery(this).addClass('bottom-on');
-      }else{
-        jQuery(this).removeClass('bottom-on');
-      }
-    });
-
-    jQuery(".js-scroll-togled[data-toggled-timing=both]").each(function(){
-       if( jQuery(window).scrollTop() - Number( jQuery(this).offset().top ) > Number( jQuery(this).attr('data-top-toggled-delay') ) ){
-         jQuery(this).addClass('top-on');
-       }else{
-         jQuery(this).removeClass('top-on');
-       }
-
-       if( (jQuery(window).scrollTop() + window.innerHeight ) - ( Number( jQuery(this).offset().top ) + jQuery(this).height() ) > Number( jQuery(this).attr('data-bottom-toggled-delay') ) ){
-         jQuery(this).addClass('bottom-on');
-       }else{
-         jQuery(this).removeClass('bottom-on');
-       }
-    });
-  };
-
-  jQuery(window).on("load",function(){
-    jQuery(".js-scroll-togled").each(function(){
-      var toggled_timing = jQuery(this).attr('data-toggled-timing');
-      if( toggled_timing === undefined || ( toggled_timing !== 'page-top' && toggled_timing !== 'page-bottom' && toggled_timing !== 'both' ) ){
-        jQuery(this).attr( 'data-toggled-timing', 'page-top' );
-      }
-      if( toggled_timing === 'page-top' ){
-        if( jQuery(this).attr('data-top-toggled-delay') === undefined ){
-          jQuery(this).attr( 'data-top-toggled-delay', 0 );
-        }
-      }else if( toggled_timing === 'page-bottom' ){
-        if( jQuery(this).attr('data-bottom-toggled-delay') === undefined ){
-          jQuery(this).attr( 'data-bottom-toggled-delay', 0 );
-        }
-      }else{
-        if( jQuery(this).attr('data-top-toggled-delay') === undefined ){
-          jQuery(this).attr( 'data-top-toggled-delay', 0 );
-        }
-        if( jQuery(this).attr('data-bottom-toggled-delay') === undefined ){
-          jQuery(this).attr( 'data-bottom-toggled-delay', 0 );
-        }
-      }
-    });
-    scroll_toggled_event();
-  });
-
-  jQuery(window).scroll(function(){
-    scroll_toggled_event();
-  });
-
-});
+// jQuery(function(){
+//   var scroll_toggled_event = function(){
+//
+//     jQuery(".js-scroll-togled[data-toggled-timing=page-top]").each(function(){
+//        if( jQuery(window).scrollTop() - Number( jQuery(this).offset().top ) > Number( jQuery(this).attr('data-top-toggled-delay') ) ){
+//          jQuery(this).addClass('top-on');
+//        }else{
+//          jQuery(this).removeClass('top-on');
+//        }
+//     });
+//
+//     jQuery(".js-scroll-togled[data-toggled-timing=page-bottom]").each(function(){
+//       if( (jQuery(window).scrollTop() + window.innerHeight ) - ( Number( jQuery(this).offset().top ) + jQuery(this).height() ) > Number( jQuery(this).attr('data-bottom-toggled-delay') ) ){
+//         jQuery(this).addClass('bottom-on');
+//       }else{
+//         jQuery(this).removeClass('bottom-on');
+//       }
+//     });
+//
+//     jQuery(".js-scroll-togled[data-toggled-timing=both]").each(function(){
+//        if( jQuery(window).scrollTop() - Number( jQuery(this).offset().top ) > Number( jQuery(this).attr('data-top-toggled-delay') ) ){
+//          jQuery(this).addClass('top-on');
+//        }else{
+//          jQuery(this).removeClass('top-on');
+//        }
+//
+//        if( (jQuery(window).scrollTop() + window.innerHeight ) - ( Number( jQuery(this).offset().top ) + jQuery(this).height() ) > Number( jQuery(this).attr('data-bottom-toggled-delay') ) ){
+//          jQuery(this).addClass('bottom-on');
+//        }else{
+//          jQuery(this).removeClass('bottom-on');
+//        }
+//     });
+//   };
+//
+//   jQuery(window).on("load",function(){
+//     jQuery(".js-scroll-togled").each(function(){
+//       var toggled_timing = jQuery(this).attr('data-toggled-timing');
+//       if( toggled_timing === undefined || ( toggled_timing !== 'page-top' && toggled_timing !== 'page-bottom' && toggled_timing !== 'both' ) ){
+//         jQuery(this).attr( 'data-toggled-timing', 'page-top' );
+//       }
+//       if( toggled_timing === 'page-top' ){
+//         if( jQuery(this).attr('data-top-toggled-delay') === undefined ){
+//           jQuery(this).attr( 'data-top-toggled-delay', 0 );
+//         }
+//       }else if( toggled_timing === 'page-bottom' ){
+//         if( jQuery(this).attr('data-bottom-toggled-delay') === undefined ){
+//           jQuery(this).attr( 'data-bottom-toggled-delay', 0 );
+//         }
+//       }else{
+//         if( jQuery(this).attr('data-top-toggled-delay') === undefined ){
+//           jQuery(this).attr( 'data-top-toggled-delay', 0 );
+//         }
+//         if( jQuery(this).attr('data-bottom-toggled-delay') === undefined ){
+//           jQuery(this).attr( 'data-bottom-toggled-delay', 0 );
+//         }
+//       }
+//     });
+//     scroll_toggled_event();
+//   });
+//
+//   jQuery(window).scroll(function(){
+//     scroll_toggled_event();
+//   });
+//
+// });
 
 /* ================================
 * スクロール位置が特定範囲内の場合、on を付与
 * .js-scroll-area-togledが付与された要素に対して動作する
 * data-area=""に判定するセレクタを設定（例：#◯◯◯◯）
 ================================ */
-jQuery(function(){
-  jQuery(window).scroll(function(){
-    jQuery(".js-scroll-area-togled").each(function(){
-
-      var target = jQuery(this).attr('data-area');
-
-      if(jQuery(window).scrollTop() - jQuery(target).offset().top >= 0){
-        jQuery(this).addClass('on');
-      }
-
-      if(jQuery(window).scrollTop() - ( jQuery(target).offset().top + jQuery(target).height() ) >= 0 ){
-        jQuery(this).removeClass('on');
-      }
-
-      if(jQuery(window).scrollTop() - jQuery(target).offset().top < 0){
-        jQuery(this).removeClass('on');
-      }
-
-    });
-  });
-});
+// jQuery(function(){
+//   jQuery(window).scroll(function(){
+//     jQuery(".js-scroll-area-togled").each(function(){
+//
+//       var target = jQuery(this).attr('data-area');
+//
+//       if(jQuery(window).scrollTop() - jQuery(target).offset().top >= 0){
+//         jQuery(this).addClass('on');
+//       }
+//
+//       if(jQuery(window).scrollTop() - ( jQuery(target).offset().top + jQuery(target).height() ) >= 0 ){
+//         jQuery(this).removeClass('on');
+//       }
+//
+//       if(jQuery(window).scrollTop() - jQuery(target).offset().top < 0){
+//         jQuery(this).removeClass('on');
+//       }
+//
+//     });
+//   });
+// });
